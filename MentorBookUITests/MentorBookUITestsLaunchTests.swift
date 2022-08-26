67d9f18e627f1100cf452d6e63afdb2f7f8cb9b2
ShoWatanabe <nabesho017@gmail.com>
2022-08-26 17:31:53 +0900

    Initial Commit

---
+++ MentorBookUITests/MentorBookUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MentorBookUITestsLaunchTests.swift
+//  MentorBookUITests
+//
+//  Created by 渡邉昇 on 2022/08/26.
+//
+
+import XCTest
+
+class MentorBookUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
